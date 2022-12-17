8f429bd675edf424fe618aa80350bfda80489e8c
Hussien <103529817+hussienzy@users.noreply.github.com>
2022-12-17 13:12:29 +0800

    Create knight_rider.ino

---
+++ Project/Project 1 :Knight Rider/knight_rider.ino
@@ -0,0 +1,111 @@
+int ledPin1 = 16; 
+int ledPin2 = 19;
+int ledPin3 = 17;
+int ledPin4 = 9;
+int ledPin5 = 8;
+int ledPin6 = 7;
+int ledPin7 = 6;
+int ledPin8 = 5; 
+int ledPin9 = 4;
+int ledPin10 = 3;
+const int delayTime = 50; 
+
+ void setup () 
+ 
+{
+  pinMode(ledPin1, OUTPUT); 
+  pinMode(ledPin2, OUTPUT); 
+  pinMode(ledPin3, OUTPUT); 
+  pinMode(ledPin4, OUTPUT); 
+  pinMode(ledPin5, OUTPUT); 
+  pinMode(ledPin6, OUTPUT); 
+  pinMode(ledPin7, OUTPUT); 
+  pinMode(ledPin8, OUTPUT);
+  pinMode(ledPin9, OUTPUT);
+   pinMode(ledPin10, OUTPUT);
+} 
+ void loop() { 
+ 
+ 
+ digitalWrite(ledPin1,HIGH);
+ delay(delayTime);
+ digitalWrite(ledPin1,LOW);
+ digitalWrite(ledPin2,HIGH);
+ delay(delayTime); 
+ digitalWrite(ledPin2,LOW);
+ 
+ digitalWrite(ledPin3,HIGH);
+ delay(delayTime);
+ digitalWrite(ledPin3,LOW);
+ 
+ digitalWrite(ledPin4,HIGH);
+ delay(delayTime);
+ digitalWrite(ledPin4,LOW);
+
+ digitalWrite(ledPin5,HIGH);
+ delay(delayTime);
+ digitalWrite(ledPin5,LOW);
+ 
+ digitalWrite(ledPin6,HIGH);
+ delay(delayTime);
+ digitalWrite(ledPin6,LOW);
+ 
+ digitalWrite(ledPin7,HIGH);
+ delay(delayTime);
+ digitalWrite(ledPin7,LOW);
+ 
+ digitalWrite(ledPin8,HIGH);
+ delay(delayTime);
+ digitalWrite(ledPin8,LOW);
+ 
+  digitalWrite(ledPin9,HIGH);
+ delay(delayTime);
+ digitalWrite(ledPin9,LOW);
+
+ digitalWrite(ledPin10,HIGH);
+ delay(delayTime);
+ digitalWrite(ledPin10,LOW);
+ 
+ digitalWrite(ledPin10,HIGH);
+ delay(delayTime); 
+ digitalWrite(ledPin10,LOW);
+
+ digitalWrite(ledPin9,HIGH);
+ delay(delayTime); 
+ digitalWrite(ledPin9,LOW);
+ 
+ digitalWrite(ledPin8,HIGH);
+ delay(delayTime); 
+ digitalWrite(ledPin8,LOW);
+ digitalWrite(ledPin7,HIGH);
+ delay(delayTime); 
+ digitalWrite(ledPin7,LOW);
+ 
+ digitalWrite(ledPin6,HIGH); 
+ delay(delayTime); 
+ digitalWrite(ledPin6,LOW); 
+ 
+ digitalWrite(ledPin5,HIGH);
+ delay(delayTime); 
+ digitalWrite(ledPin5,LOW); 
+ 
+ digitalWrite(ledPin4,HIGH); 
+ delay(delayTime); 
+ digitalWrite(ledPin4,LOW); 
+ 
+ digitalWrite(ledPin3,HIGH); 
+ delay(delayTime); 
+ digitalWrite(ledPin3,LOW); 
+ 
+ digitalWrite(ledPin2,HIGH); 
+ delay(delayTime); 
+ digitalWrite(ledPin2,LOW); 
+ 
+ digitalWrite(ledPin1,HIGH);
+ delay(delayTime); 
+ digitalWrite(ledPin1,LOW); 
+ 
+ 
+}
+
+ 
